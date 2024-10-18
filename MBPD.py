1b291c23582b80a9ab055d77e55a563c34bf3c9d
LorMeBioAI <103401277+LorMeBioAI@users.noreply.github.com>
2024-10-18 22:57:02 +0800

    Update MBPD.py

--- MBPD.py
+++ MBPD.py
@@ -5,148 +5,61 @@ import sys
 import time
 import subprocess
 
-"""
-添加参数模块
-"""
+# 添加参数模块
 parser = argparse.ArgumentParser(description='BacPD')
-parser.add_argument('--file',help='Sample table')
-parser.add_argument('--pwd', type=str, default='./',help='Output path, default is current path')
-parser.add_argument('--similarity', type=float, default=0.9,help='Sequencing regions of V1-V2 or V4 are recommended using 0.9,others are ecommended using 0.8,default=0.8')
+parser.add_argument('--file', help='Sample table')
+parser.add_argument('--pwd', type=str, default='./', help='Output path, default is current path')
+parser.add_argument('--similarity', type=float, default=0.9, 
+                    help='Recommended similarity: 0.9 for V1-V2/V4, 0.8 for others, default=0.8')
 args = parser.parse_args()
-
 
 def progress_bar():
     for i in range(1, 101):
         print("\r", end="")
-        print("Running progress: {}%: ".format(i), "▋" * (i // 2), end="")
+        print(f"Running progress: {i}%: ", "▋" * (i // 2), end="")
         sys.stdout.flush()
-        time.sleep(0.0005)
+        time.sleep(0.005)
 
 def mkdir(path):
-    folder = os.path.exists(path)
-
-    if not folder:  # 判断是否存在文件夹如果不存在则创建为文件夹
-        os.makedirs(path)  # 创建文件时如果路径不存在会创建这个路径
-        print("---  new folder...  ---")
-
+    if not os.path.exists(path):
+        os.makedirs(path)
+        print(f"--- Created new folder: {path} ---")
     else:
-        print("---  There is this folder!  ---")
+        print(f"--- Folder already exists: {path} ---")
 
+# 获取软件路径，转换为绝对路径
+abs_path = os.path.split(os.path.realpath(__file__))[0]
+abs_raw = os.path.realpath(args.file)
+abs_db = os.path.join(abs_path, "db")
+abs_bin = os.path.join(abs_path, "bin")
+similarity = args.similarity
 
-"""
-获取软件的路径
-一定要注意先把相对路径转成绝对路径，不然后面进入输出文件夹就会出问题！
-"""
-abs=os.path.split(os.path.realpath(__file__))[0]
-abs_raw=os.path.realpath(args.file)
-abs_db=abs+"/db"
-abs_bin=abs+"/bin"
-similarity=args.similarity
 print(f"""
-The PWD of workplace is: {abs}
+The PWD of workplace is: {abs_path}
 The PWD of raw.fq.list is: {abs_raw}
 """)
 
-
-"""
-创建文件夹，定义输出路径
-"""
-
-# 先读取进来，不然路径不对的话可能会出问题，相对路径发生改变了
-
-
-# raw_fq_list = pd.read_csv("./Example/raw.fq.list",sep="\t",header=None)
-
-# 创建这个路径，并在这个路径下操作任务
+# 创建文件夹和输出路径
 mkdir(args.pwd)
 os.chdir(args.pwd)
 mkdir("00.Data")
 mkdir("01.ASV.tax")
 mkdir("shell")
 
-
-# 读入数据表的绝对路径！
-raw_fq_list = pd.read_csv(abs_raw,sep="\t",header=None)
+# 读入数据表
+raw_fq_list = pd.read_csv(abs_raw, sep="\t", header=None)
 ID_list = raw_fq_list[0]
 fq_list = raw_fq_list[1]
 num = ID_list.shape[0]
 
-# 常用缩写设置
-PWD=os.getcwd()+"/"
-abs_shell=PWD+"shell/"
-mode = PWD+"shell/"+'S01.1.{}'
+# 常用路径设置
+PWD = os.getcwd() + "/"
+abs_shell = PWD + "shell/"
+mode = PWD + "shell/S01.1.{}"
 
-"""
-定义dada2文件
-"""
-
-f = open(mode.format("dada2") + '.r', 'w', encoding='utf-8')
-content=(f"""
-library(Rcpp)
-library(crayon)
-library(withr)
-library(ggplot2)
-library(BiocGenerics)
-library(S4Vectors)
-library(IRanges)
-library(XVector)
-library(GenomeInfoDb)
-library(matrixStats)
-library(Biobase)
-library(Matrix)
-library(latticeExtra)
-library(reshape2)
-library(dada2)
-##----filenames-------------
-path <- "{PWD}00.Data"
-fns <- list.files(path, pattern=".fq.gz")
-sample.names <- sapply(strsplit(basename(fns), "[.]"), `[`, 1)
-sample.names
-##----filter and trim-------
-filtpath <- file.path(path, "filtered")
-filterAndTrim(file.path(path,fns), file.path(filtpath,fns),  maxEE=1, truncQ=2, rm.phix=TRUE, compress=TRUE, verbose=TRUE, multithread=TRUE)
-filts <- list.files(filtpath, pattern="fq", full.names=TRUE)
-names(filts) <- sample.names
-##----learn Errors rate----------
-set.seed(100)
-err <- learnErrors(filts, nbases = 1e8, multithread=TRUE, randomize=TRUE)
-##----Infer sequence variants----
-dds <- vector("list", length(sample.names))
-names(dds) <- sample.names
-for(sam in sample.names) {{
-	cat("Processing:", sam, "\\n")
-	derep <- derepFastq(filts[[sam]])  ##Dereplication
-	dds[[sam]] <- dada(derep, err=err, multithread=TRUE)  ##dada2
-}}
-##----Construct sequence table---
-seqtab <- makeSequenceTable(dds)
-saveRDS(seqtab, "./seqtab.rds")
-write.table(t(seqtab),"table.xls",sep="/\t")
-# Merge multiple runs (if necessary)
-#st1 <- readRDS("path/to/run1/output/seqtab.rds")
-#st2 <- readRDS("path/to/run2/output/seqtab.rds")
-#st3 <- readRDS("path/to/run3/output/seqtab.rds")
-#st.all <- mergeSequenceTables(st1, st2, st3)
-##----Remove chimeras------------
-seqtabrechim <- removeBimeraDenovo(seqtab, method="consensus", multithread=TRUE)
-write.table(t(seqtabrechim),"table.rechim.xls",sep="\t")
-""")
-f.write(content)
-f.close()
-
-"""
-定义脚本文件
-"""
-f = open(mode.format("pathogen") + '.sh', 'w', encoding='utf-8')
-for i in range(0,num,1):
-  # TODO: 循环输出脚本...
-  fq = fq_list[i]
-  ID = ID_list[i]
-  f.write(f'ln -s {fq} {PWD}00.Data/{ID}.fq.gz')
-  f.write("\n")
-
-
-content = (f"""
+# 生成第一部分脚本 (S01.1.pathogen.part1.sh)
+with open(mode.format("pathogen.part1") + '.sh', 'w', encoding='utf-8') as f1:
+    content1 = f"""
 cd {PWD}01.ASV.tax/
 Rscript {abs_shell}S01.1.dada2.r
 perl {abs_bin}/dada2normal.pl table.rechim.xls
@@ -154,23 +67,67 @@ perl {abs_bin}/asv-generate.pl table.rechim.xls asv.table asv.fa
 sed -i 's/"//g' asv_table.tmp.xls
 {abs_bin}/sample_order.pl asv_table.tmp.xls {abs_raw} asv_table.xls
 rm asv_table.tmp.xls
+
+echo "First part of the script completed successfully."
+"""
+    f1.write(content1)
+
+# 生成第二部分脚本 (S01.1.pathogen.part2.sh)
+with open(mode.format("pathogen.part2") + '.sh', 'w', encoding='utf-8') as f2:
+    content2 = f"""
+cd {PWD}01.ASV.tax/
 ####assign_taxonomy.py depends on qiime1; conda activate qiime1######
 assign_taxonomy.py -m uclust -i asv_rep.fasta --similarity {similarity} -r {abs_db}/pathogen.fasta -t {abs_db}/pathogen.tax -o assign_taxonomy
 {abs_bin}/taxaTable_byAss.pl asv_table.xls assign_taxonomy/asv_rep_tax_assignments.txt asv_taxa_table.xls
 biom convert -i asv_taxa_table.xls -o asv_taxa_table.biom --process-obs-metadata taxonomy --table-type "OTU table" --to-json
 rm -r assign_taxonomy
 
+echo "Second part of the script completed successfully."
+"""
+    f2.write(content2)
 
-""")
-f.write(content)
-f.close()
-# 显示运行成功！
-print("""
+# 生成 R 脚本 (S01.1.dada2.r)
+with open(mode.format("dada2") + '.r', 'w', encoding='utf-8') as r_file:
+    r_content = f"""
+library(dada2)
+path <- "{PWD}00.Data"
+fns <- list.files(path, pattern=".fq.gz")
+sample.names <- sapply(strsplit(basename(fns), "[.]"), `[`, 1)
+filtpath <- file.path(path, "filtered")
+filterAndTrim(file.path(path, fns), file.path(filtpath, fns), maxEE=1, truncQ=2, rm.phix=TRUE, compress=TRUE, multithread=TRUE)
+filts <- list.files(filtpath, pattern="fq", full.names=TRUE)
+names(filts) <- sample.names
+set.seed(100)
+err <- learnErrors(filts, nbases = 1e8, multithread=TRUE)
+dds <- lapply(sample.names, function(sam) {{
+    derep <- derepFastq(filts[[sam]])
+    dada(derep, err=err, multithread=TRUE)
+}})
+seqtab <- makeSequenceTable(dds)
+saveRDS(seqtab, "./seqtab.rds")
+write.table(t(seqtab), "table.xls", sep="\t")
+seqtabrechim <- removeBimeraDenovo(seqtab, method="consensus", multithread=TRUE)
+write.table(t(seqtabrechim), "table.rechim.xls", sep="\t")
+"""
+    r_file.write(r_content)
 
-""")
-print(f"""
-Please note that your input！！！
-file:{args.file}
-pwd:{args.pwd}
-""")
+# 生成符号链接的脚本
+with open(mode.format("symbolic_links") + '.sh', 'w', encoding='utf-8') as f_link:
+    for i in range(num):
+        f_link.write(f'ln -s {fq_list[i]} {PWD}00.Data/{ID_list[i]}.fq.gz\n')
+
+# 运行第一部分和第二部分脚本
+def run_shell_script(script):
+    result = subprocess.run(['bash', script], capture_output=True, text=True)
+    print(result.stdout)
+    if result.returncode != 0:
+        print(f"Error running {script}:\n{result.stderr}")
+
+print("Running first part of the script...")
+run_shell_script(mode.format("pathogen.part1") + '.sh')
+
+print("First part completed. Now running the second part...")
+run_shell_script(mode.format("pathogen.part2") + '.sh')
+
+print("All tasks completed successfully!")
 progress_bar()
